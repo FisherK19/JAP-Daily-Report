6e2815180bb429c8d89e150182f58f7140768232
kristie fisher <fisherkristie19@icloud.com>
2024-05-21 18:58:46 -0400

    cahnged daily report

--- routes/dailyReport.js
+++ routes/dailyReport.js
@@ -12,7 +12,8 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const {
         date, job_number, contract, foreman, cell_number, customer, customer_po,
-        job_site, job_description, job_completion, material_description, equipment_description,
+        job_site, job_description, job_completion, siding, roofing, flashing, miscellaneous,
+        trucks, welders, generators, compressors, fuel, scaffolding, safety_equipment, miscellaneous_equipment,
         hours_worked, employee, straight_time, double_time, time_and_a_half,
         emergency_purchases, approved_by
     } = req.body;
@@ -22,15 +23,17 @@ router.post('/', (req, res) => {
     const sql = `
         INSERT INTO daily_reports (
             date, job_number, contract, foreman, cell_number, customer, customer_po,
-            job_site, job_description, job_completion, material_description, equipment_description,
+            job_site, job_description, job_completion, siding, roofing, flashing, miscellaneous,
+            trucks, welders, generators, compressors, fuel, scaffolding, safety_equipment, miscellaneous_equipment,
             hours_worked, employee, straight_time, double_time, time_and_a_half,
             emergency_purchases, approved_by
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     const values = [
         date, job_number, contract ? 1 : 0, foreman, cell_number, customer, customer_po,
-        job_site, job_description, job_completion, material_description, equipment_description,
+        job_site, job_description, job_completion, siding, roofing, flashing, miscellaneous,
+        trucks, welders, generators, compressors, fuel, scaffolding, safety_equipment, miscellaneous_equipment,
         hours_worked, employee, straight_time, double_time, time_and_a_half,
         emergency_purchases, approved_by
     ];
@@ -45,3 +48,4 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
