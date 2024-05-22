8db77be357de9d61ba7b41a78b2573997eb13f5a
kristie fisher <fisherkristie19@icloud.com>
2024-05-22 19:35:30 -0400

    added column values to match

--- routes/dailyReport.js
+++ routes/dailyReport.js
@@ -13,8 +13,9 @@ router.post('/', async (req, res) => {
     try {
         const {
             date, job_number, t_and_m, contract, foreman, cell_number, customer, customer_po,
-            job_site, job_description, job_completion, material_description, equipment_description,
-            hours_worked, employee, straight_time, double_time, time_and_a_half,
+            job_site, job_description, job_completion, siding, roofing, flashing, miscellaneous,
+            trucks, welders, generators, compressors, fuel, scaffolding, safety_equipment, miscellaneous_equipment,
+            material_description, equipment_description, hours_worked, employee, straight_time, double_time, time_and_a_half,
             emergency_purchases, approved_by, shift_start_time, temperature_humidity, report_copy,
             manlifts_equipment, manlifts_fuel, delay_lost_time, employees_off, sub_contract
         } = req.body;
@@ -24,17 +25,19 @@ router.post('/', async (req, res) => {
         const sql = `
             INSERT INTO daily_reports (
                 date, job_number, t_and_m, contract, foreman, cell_number, customer, customer_po,
-                job_site, job_description, job_completion, material_description, equipment_description,
-                hours_worked, employee, straight_time, double_time, time_and_a_half,
+                job_site, job_description, job_completion, siding, roofing, flashing, miscellaneous,
+                trucks, welders, generators, compressors, fuel, scaffolding, safety_equipment, miscellaneous_equipment,
+                material_description, equipment_description, hours_worked, employee, straight_time, double_time, time_and_a_half,
                 emergency_purchases, approved_by, shift_start_time, temperature_humidity, report_copy,
                 manlifts_equipment, manlifts_fuel, delay_lost_time, employees_off, sub_contract
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         `;
 
         const values = [
             date, job_number, t_and_m ? 1 : 0, contract ? 1 : 0, foreman, cell_number, customer, customer_po,
-            job_site, job_description, job_completion, material_description, equipment_description,
-            hours_worked, employee, straight_time, double_time, time_and_a_half,
+            job_site, job_description, job_completion, siding, roofing, flashing, miscellaneous,
+            trucks, welders, generators, compressors, fuel, scaffolding, safety_equipment, miscellaneous_equipment,
+            material_description, equipment_description, hours_worked, employee, straight_time, double_time, time_and_a_half,
             emergency_purchases, approved_by, shift_start_time, temperature_humidity, report_copy,
             manlifts_equipment, manlifts_fuel, delay_lost_time, employees_off, sub_contract
         ];
