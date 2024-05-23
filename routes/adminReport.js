9292ab3e21e8abbd7b117530f0c97e43979f6192
kristie fisher <fisherkristie19@icloud.com>
2024-05-23 17:30:55 -0400

    debugging

--- routes/adminReport.js
+++ routes/adminReport.js
@@ -4,7 +4,6 @@ const { pool } = require('../config/connection');
 const PDFDocument = require('pdfkit');
 const nodemailer = require('nodemailer');
 const ExcelJS = require('exceljs');
-const fs = require('fs');
 
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
@@ -51,11 +50,29 @@ function generatePDF(reports, username, res) {
         doc.text(`Customer PO: ${report.customer_po}`);
         doc.text(`Job Site: ${report.job_site}`);
         doc.text(`Job Description: ${report.job_description}`);
+        doc.text(`Sheeting / Materials: ${report.materials}`);
         doc.text(`Hours Worked: ${report.hours_worked}`);
         doc.text(`Employee: ${report.employee}`);
         doc.text(`Straight Time: ${report.straight_time}`);
         doc.text(`Time and a Half: ${report.time_and_a_half}`);
         doc.text(`Double Time: ${report.double_time}`);
+        doc.text(`Trucks: ${report.trucks}`);
+        doc.text(`Welders: ${report.welders}`);
+        doc.text(`Generators: ${report.generators}`);
+        doc.text(`Compressors: ${report.compressors}`);
+        doc.text(`Company Fuel: ${report.fuel}`);
+        doc.text(`Scaffolding: ${report.scaffolding}`);
+        doc.text(`Safety Equipment: ${report.safety_equipment}`);
+        doc.text(`Miscellaneous: ${report.miscellaneous_equipment}`);
+        doc.text(`Manlifts / Rentals: ${report.manlifts_equipment}`);
+        doc.text(`Rental Fuel: ${report.manlifts_fuel}`);
+        doc.text(`Sub-Contract: ${report.sub_contract}`);
+        doc.text(`Emergency Purchases: ${report.emergency_purchases}`);
+        doc.text(`Delay / Lost Time: ${report.delay_lost_time}`);
+        doc.text(`Employees Off: ${report.employees_off}`);
+        doc.text(`Temperature/Humidity: ${report.temperature_humidity}`);
+        doc.text(`Approved By: ${report.approved_by}`);
+        doc.text(`Report Copy: ${report.report_copy}`);
         doc.moveDown();
     });
 
@@ -76,11 +93,29 @@ function generateExcel(reports, username, res) {
         { header: 'Customer PO', key: 'customer_po', width: 15 },
         { header: 'Job Site', key: 'job_site', width: 15 },
         { header: 'Job Description', key: 'job_description', width: 30 },
+        { header: 'Sheeting / Materials', key: 'materials', width: 30 },
         { header: 'Hours Worked', key: 'hours_worked', width: 15 },
         { header: 'Employee', key: 'employee', width: 15 },
         { header: 'Straight Time', key: 'straight_time', width: 15 },
         { header: 'Time and a Half', key: 'time_and_a_half', width: 15 },
-        { header: 'Double Time', key: 'double_time', width: 15 }
+        { header: 'Double Time', key: 'double_time', width: 15 },
+        { header: 'Trucks', key: 'trucks', width: 15 },
+        { header: 'Welders', key: 'welders', width: 15 },
+        { header: 'Generators', key: 'generators', width: 15 },
+        { header: 'Compressors', key: 'compressors', width: 15 },
+        { header: 'Company Fuel', key: 'fuel', width: 15 },
+        { header: 'Scaffolding', key: 'scaffolding', width: 15 },
+        { header: 'Safety Equipment', key: 'safety_equipment', width: 15 },
+        { header: 'Miscellaneous Equipment', key: 'miscellaneous_equipment', width: 15 },
+        { header: 'Manlifts / Rentals', key: 'manlifts_equipment', width: 15 },
+        { header: 'Rental Fuel', key: 'manlifts_fuel', width: 15 },
+        { header: 'Sub-Contract', key: 'sub_contract', width: 30 },
+        { header: 'Emergency Purchases', key: 'emergency_purchases', width: 30 },
+        { header: 'Delay / Lost Time', key: 'delay_lost_time', width: 30 },
+        { header: 'Employees Off', key: 'employees_off', width: 30 },
+        { header: 'Temperature/Humidity', key: 'temperature_humidity', width: 30 },
+        { header: 'Approved By', key: 'approved_by', width: 15 },
+        { header: 'Report Copy', key: 'report_copy', width: 15 }
     ];
 
     reports.forEach(report => {
