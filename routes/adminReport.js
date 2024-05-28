a7cf1bec8a69d891dc9af45ec9f02df858dc5e30
kristie fisher <fisherkristie19@icloud.com>
2024-05-27 23:37:34 -0400

    corrected path to image

--- routes/adminReport.js
+++ routes/adminReport.js
@@ -43,7 +43,7 @@ function generatePDF(reports, username, res) {
     doc.pipe(res);
 
     // Header
-    doc.image('path/to/logo.png', { width: 50, align: 'center' })
+    doc.image('../assets/images/company-logo.png', { width: 50, align: 'center' })
         .fontSize(20)
         .text('JOHN A. PAPALAS & COMPANY', { align: 'center' })
         .fontSize(12)
